49da0772a41ec20f666c49cb5c3affe8d112f8da
markokarajlovic <markokarajlovic@gmail.com>
2018-11-15 12:16:15 +0100

    2

--- app/src/main/java/com/ephoenixdev/izgubljenonadjeno/MainActivity.java
+++ app/src/main/java/com/ephoenixdev/izgubljenonadjeno/MainActivity.java
@@ -1,28 +1,27 @@
 package com.ephoenixdev.izgubljenonadjeno;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.btnMain);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
+                finish();
+
             }
-        });
+            },SPLASH_TIME_OUT);
     }
 }
